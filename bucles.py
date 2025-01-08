ad9eb1b9cd23397b329f28eefbec74a30225ff1b
javi-r-p <7460103@alu.murciaeduca.es>
2025-01-08 18:15:25 +0100

    Condicionales más avanzados y bucles for y while

---
+++ bucles.py
@@ -0,0 +1,9 @@
+for i in range(11):
+    print(i)
+
+print ("\n")
+
+contador = 0
+while contador <= 10:
+    print(contador)
+    contador += 1
--- condicionales.py
+++ condicionales.py
@@ -3,3 +3,11 @@ if edad >= 18:
     print("Eres mayor de edad")
 else:
     print("No eres mayor de edad")
+
+num1 = int(input("Introduce un número: "))
+num2 = int(input("Introduce otro número: "))
+if (num1 < num2) and (num1 > 5):
+    total = num1 + num2
+    print("El resultado de la suma es:",total)
+else:
+    print("El operando 1 es mayor que el 2, no se puede calcular la suma")

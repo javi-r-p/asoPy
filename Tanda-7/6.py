218f30b8e5e09910130a2967846f1285cb0e157c
javi-r-p <7460103@alu.murciaeduca.es>
2025-01-22 19:29:43 +0100

    Tanda 7 de ejercicios - 4

--- Tanda-7/6.py
+++ Tanda-7/6.py
@@ -7,7 +7,6 @@ archivo = Path("inventario.txt")
 print(" --- MENÚ ---")
 print("1. Ver inventario")
 print("2. Añadir un objeto al inventario")
-#print("3. Eliminar un objeto del inventario")
 opcion = int(input("Selecciona una opción: "))
 
 if opcion == 1:
@@ -19,11 +18,8 @@ if opcion == 1:
 elif opcion == 2:
     print("Has elegido añadir un objeto al inventario.")
     objetoNuevo = input("Introduce el objeto que quieres añadir: ")
-    archivo = open("inventario.txt", "a")
-    archivo.write_text(objetoNuevo)
-#elif opcion == 3:
-#   print("Has elegido eliminar un objeto del inventario.")
-#   elementoEliminar = input("Introduce el objeto que quieres eliminar: ")
+    with archivo.open("a") as archivo:
+        archivo.write(objetoNuevo+"\n")
 else:
     print("No has seleccionado una opción válida.")
     exit(1)
---
+++ Tanda-7/7.py
@@ -0,0 +1,6 @@
+#Conversión de unidades de temperatura
+
+print("Selecciona una opción:")
+print("1. Fahrenheit a Celsius")
+print("2. Celsius a Fahrenheit")
+opcion = int(input("Selecciona una opción: "))

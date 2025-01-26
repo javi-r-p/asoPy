f0c5089e90139165d89fed29c2a76b368151df26
Javier Rodríguez Parra <117043352+javi-r-p@users.noreply.github.com>
2025-01-26 12:28:46 +0100

    Tanda 7 de ejercicios - 5

--- Tanda-7/7.py
+++ Tanda-7/7.py
@@ -1,6 +1,18 @@
 #Conversión de unidades de temperatura
 
 print("Selecciona una opción:")
-print("1. Fahrenheit a Celsius")
-print("2. Celsius a Fahrenheit")
+print("1. Celsius a Fahrenheit")
+print("2. Fahrenheit a Celsius")
 opcion = int(input("Selecciona una opción: "))
+
+if opcion == 1:
+    celsius = float(input("Introduce la temperatura en grados Celsius: "))
+    fahrenheit = (celsius * 9/5) + 32
+    print(f"{celsius}ºC son {fahrenheit}ºF")
+elif opcion == 2:
+    fahrenheit = float(input("Introduce la temperatura en grados Fahrenheit: "))
+    celsius = (fahrenheit - 32) * 5/9
+    print(f"{fahrenheit}ºF son {celsius}ºC")
+else:
+    print("No has seleccionado ninguna opción válida")
+    exit(1)
